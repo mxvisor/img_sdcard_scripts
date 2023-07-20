0685f237d7772d4692462c72338eab289a8c36f2
Vladimir <visor@nosuchmail.ru>
2023-07-20 11:57:03 +0300

    полезные скрипты для работы с внешними накопителями и образами

---
+++ select_external_drive.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+dev_list=(`lsblk -A -d -o path,tran |grep usb |cut -d ' ' -f 1`)
+
+len=${#dev_list[@]}
+
+[[ $len != 0 ]] || { echo "ERROR: No external devices available"; exit 1; }
+
+[[ $len == 1 ]] && { echo "${dev_list[0]}"; exit 0; }
+
+
+for ((i=0;i<$len;i++));do
+    #echo [$i] ${dev_list[$i]} "("`lsblk -d -n -o size ${dev_list[$i]}`")"
+    #printf "[$i] ${dev_list[$i]} (`lsblk -d -n -o size ${dev_list[$i]}`)\n"
+    LIST=$LIST"[$((i+1))] ${dev_list[$i]} (`lsblk -d -n -o size ${dev_list[$i]}`)"$'\n'
+done
+
+read -p "$LIST""Select drive [1-$((len))]: " index
+
+re='^[1-9]+$'
+[[ $index =~ $re ]] || { echo "ERROR: Entered value is not number"; exit 1; }
+
+index=$index-1
+
+[[ ! -z ${dev_list[index]} ]] || { echo "ERROR: Entered number out of range";exit 1; }
+
+echo "${dev_list[index]}"
+exit 0
---
+++ select_image.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+img_list=(`ls -1 *.{img,ext4,img.gz,ext4.gz} 2>/dev/null`)
+if [ -z "$img_list" ];then
+echo "ERROR: No images found in current directory"
+exit 1
+fi
+
+len=${#img_list[@]}
+
+for ((i=0;i<$len;i++));do
+    LIST=$LIST"[$((i+1))] ${img_list[$i]}"$'\n'
+done
+    
+read -p "$LIST""select image [1-$((len))]:" index
+
+re='^[1-9]+$'
+if ! [[ $index =~ $re ]] ; then
+    echo "ERROR: Wrong number"
+    exit 1
+fi
+
+index=$index-1
+
+if [ -z ${img_list[index]} ];then
+    echo "ERROR: Wrong image"
+    exit 1
+fi
+
+echo "${img_list[index]}"
+
+exit 0
